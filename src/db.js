848c02b877ea7ec4cdd4e9faf055a6079ab0d1b1
jose rangel <jl.rangel20@hotmail.com>
2022-09-01 12:10:54 -0500

    modificando uri

--- src/db.js
+++ src/db.js
@@ -1,22 +1,22 @@
 require('dotenv').config();
 const mongoose = require("mongoose")
-const {loaderLocation}=require("./seeders/locations")
-const {loaderCharacter}=require("./seeders/characters")
-const {loaderEpisodes}=require("./seeders/episodes")
+//const {loaderLocation}=require("./seeders/locations")
+//const {loaderCharacter}=require("./seeders/characters")
+//const {loaderEpisodes}=require("./seeders/episodes")
 
-const {MONGODBURI}=process.env;
+const {URI}=process.env
 
 const conectDB=async()=>{
 	try{
-		await mongoose.connect(MONGODBURI,{
+		await mongoose.connect(URI,{
 		useNewUrlParser: true,
     	useUnifiedTopology: true
     	});
 
 		console.log("conectada la DB de MongoDB");
-		await loaderLocation()
-		await loaderCharacter()
-		await loaderEpisodes()
+		//await loaderLocation()
+		//await loaderCharacter()
+		//await loaderEpisodes()
 
 	}catch(err){
 		console.log("se producjo el siguiente error: ", err)
